341ba416ce376272cfd76d4f19d4a811900d2b71
Ethan James <necrozma@catgirlsaresexy.org>
2024-04-09 11:15:23 -0600

    branding

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -73,7 +73,7 @@ export default function Dashboard() {
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
               <Package2 className="h-6 w-6" />
-              <span className="">Something Inc</span>
+              <span className="">Wheeler Peak Ice</span>
             </Link>
             <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
               <Bell className="h-4 w-4" />
@@ -144,7 +144,7 @@ export default function Dashboard() {
                   className="flex items-center gap-2 text-lg font-semibold"
                 >
                   <Package2 className="h-6 w-6" />
-                  <span className="sr-only">Something Inc</span>
+                  <span className="sr-only">Wheeler Peak Ice</span>
                 </Link>
                 <Button
                 onClick={() => setActivePage(1)}
