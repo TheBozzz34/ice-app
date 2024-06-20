9bf8b0e0b60eba90a7c7fd744a70311794d7955e
Ethan James <necrozma@catgirlsaresexy.org>
2024-06-19 18:42:18 -0600

    i hate ihop

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -343,18 +343,6 @@ export default function Dashboard() {
                 </TooltipTrigger>
                 <TooltipContent side="right">Employees</TooltipContent>
               </Tooltip>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Link
-                    href="#"
-                    className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                  >
-                    <LineChart className="h-5 w-5" />
-                    <span className="sr-only">Analytics</span>
-                  </Link>
-                </TooltipTrigger>
-                <TooltipContent side="right">Analytics</TooltipContent>
-              </Tooltip>
             </nav>
             <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
               <Tooltip>
