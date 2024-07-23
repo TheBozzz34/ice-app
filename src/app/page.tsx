c0a660158c1680de64bcf93210ef576d2195e0fe
Ethan James <necrozma@catgirlsaresexy.org>
2024-07-22 18:15:11 -0600

    redirect main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,13 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center p-24">
-			<div className="branding">
-				<Image
-					src="/Logo-Draft.png"
-					alt="Wheeler Peak Ice Logo"
-					className="dark:invert w-auto w-auto"
-					width={100}
-					height={24}
-					priority
-				/>
-			</div>
+import Head from 'next/head';
+import useRedirectAfterSomeSeconds from '../hooks/useRedirectAfterSomeSeconds';
 
-			<a
-				href="https://www.mysmartice.com/"
-				className="button inquiry"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				Dealer Inquiries
-			</a>
+export default function ErrorPage() {
+  const { secondsRemaining } = useRedirectAfterSomeSeconds('https://icehouseamerica.com', 1);
 
-			<a
-				href="/login"
-				className="button login"
-				target="_self"
-				rel="noopener noreferrer"
-			>
-				Login
-			</a>
-		</main>
-	);
+  return (
+    <>
+    </>
+  );
 }
