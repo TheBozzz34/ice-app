6757dc784169840e81a863f9602bf96b523bd747
Ethan James <necrozma@catgirlsaresexy.org>
2024-07-23 22:05:29 -0600

    Added a suspense boundary

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -2,22 +2,25 @@
 
 import { useSearchParams } from 'next/navigation'
 import { AlertCircle } from 'lucide-react'
+import { Suspense } from 'react'
 
 export default function ErrorPage() {
   const searchParams = useSearchParams()
   const message = searchParams.get('__message')
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <div className="bg-white p-6 rounded-lg shadow-lg text-center">
-        <AlertCircle className="h-12 w-12 text-red-600 mx-auto" />
-        <h1 className="mt-4 text-2xl font-semibold text-gray-800">An error occurred!</h1>
-        <p className="mt-2 text-gray-600">A detailed description of the error is below:</p>
-        <p className="mt-4 text-gray-800 mb-4">{message ? atob(message) : ''}</p>
-        <a className="mt-6 bg-red-600 text-white py-2 px-4 rounded hover:bg-red-700 focus:outline-none" href="/login">
-          Go to login
-        </a>
+    <Suspense>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+        <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+          <AlertCircle className="h-12 w-12 text-red-600 mx-auto" />
+          <h1 className="mt-4 text-2xl font-semibold text-gray-800">An error occurred!</h1>
+          <p className="mt-2 text-gray-600">A detailed description of the error is below:</p>
+          <p className="mt-4 text-gray-800 mb-4">{message ? atob(message) : ''}</p>
+          <a className="mt-6 bg-red-600 text-white py-2 px-4 rounded hover:bg-red-700 focus:outline-none" href="/login">
+            Go to login
+          </a>
+        </div>
       </div>
-    </div>
+    </Suspense>
   )
 }
